2360daa419c7830ad8171dc6a97f75897a7e8af8
erikmukk <34135105+erikmukk@users.noreply.github.com>
2017-11-30 23:58:56 +0200

    Revert "Revert "How to paly""
    This reverts commit ffa330b52a1861d67c02529be14915ff84201784.

--- Sudoku.py
+++ Sudoku.py
@@ -79,7 +79,21 @@ def settinguteScreen():
                     
                 heli = True
 
-
+def kuidasmängida():   #siin on pooleli
+    ekraaniPind.fill(valge)
+    #Taust
+    pilt("puit.jpg",0,0)
+    #Juhend
+    pilt("sudokujuhend.png",0,0)
+    
+    pygame.display.flip()
+    
+    while True:
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            break
+    
 def homescreen():
     #ekraani atribuudid
     pygame.display.set_caption("SuDoKu   By Uq and Erki")
@@ -91,11 +105,17 @@ def homescreen():
     #Sudoku logo
     tekstKastis("SuDoKu", "Bauhaus 93", 100, 200, 120)
     
+    #Mängima
+    tekstKastis("Mängima", "Bauhaus 93", 70, 225, 250)
+    
     #Settings nupp
-    tekstKastis("Sätted", "Bauhaus 93", 50, 300, 310)
+    tekstKastis("Sätted", "Bauhaus 93", 50, 300, 400)
     
     #Raskusastmed
     tekstKastis("Raskusastmed", "Bauhaus 93", 50, 220, 465)
+    
+    #Abimehike
+    pilt("küsimärk.png",620,620)
     
     pygame.display.flip()
     
@@ -109,9 +129,16 @@ def homescreen():
             break
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  #vasakklikk = 1
             eventx, eventy = pygame.mouse.get_pos()
-            if evendiasukoht(270,470,300,380,eventx,eventy):
+            if evendiasukoht(295,450,400,450,eventx,eventy):
                 print((eventx, eventy))
                 settinguteScreen()
+                break
+            elif evendiasukoht(220,520,250,330,eventx,eventy): #mängima
+                print(eventx,eventy)
+            elif evendiasukoht(215,530,470,515,eventx,eventy): #raskusastmed
+                print(eventx,eventy)
+            elif evendiasukoht(620,720,620,720,eventx,eventy):
+                kuidasmängida()
                 break
             else:
                 continue
